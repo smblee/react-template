5c1b90bdf42e7e82f8051b623e546fde0e6ad127
Bryan Seungmin Lee <slee@draftkings.com>
2021-02-15 14:52:45 -0800

    add loadable tests

---
+++ src/utils/__tests__/loadable.mock.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const LoadableMock = () => {
+  return <div>My lazy-loaded component</div>;
+};
+
+// eslint-disable-next-line import/no-default-export
+export default LoadableMock;
