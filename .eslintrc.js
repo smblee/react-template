73086bcfd8c3e2d9d5cccca5b1e818985c0b72ed
Bryan Seungmin Lee <slee@draftkings.com>
2021-02-15 07:11:05 -0800

    feat: Add storybook support

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,9 +21,31 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
     'no-underscore-dangle': 'off',
-    
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-unescaped-entities': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.stories.{ts,tsx}', '**/*.test.{ts,tsx}', '**/__tests__/*'],
+      },
+    ],
     // React 17
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off"
-  }
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  overrides: [
+    {
+      files: ['src/**/*.stories.{ts,tsx}'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+        'import/no-default-export': 'off',
+      },
+    },
+    {
+      files: ['**/*.{ts,tsx}'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
 };
